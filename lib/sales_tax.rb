38d27867205fba2ed0db4cf7bcef012b73bbdc83
Dirk Gadsden <dirk@esherido.com>
2016-02-07 11:34:36 -0800

    Move download task into gem

--- lib/sales_tax.rb
+++ lib/sales_tax.rb
@@ -1,3 +1,7 @@
 require 'sales_tax/version'
 require 'sales_tax/base'
 require 'sales_tax/auto_load'
+
+if defined? Rake
+  load File.join(File.dirname(__FILE__), 'sales_tax', 'tasks', 'download.rake')
+end
